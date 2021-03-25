a4acd5d1e18bb4a56c9df7a0050a3a6d06b74a1c
afoster44 <alexfost94@gmail.com>
2021-03-25 10:22:36 -0600

    AF - fully functional rental service/controllers

--- proper.server/server/models/Rental.js
+++ proper.server/server/models/Rental.js
@@ -1,5 +1,6 @@
 
 import mongoose from 'mongoose'
+import { Maintenance } from '../models/Maintenance'
 const Schema = mongoose.Schema
 const ObjectId = mongoose.SchemaTypes.ObjectId
 
@@ -15,10 +16,10 @@ const Rental = new Schema(
     rent: { type: Number, required: true },
     yearBuilt: { type: String, required: true },
     tenants: [{ type: String, ref: 'Tenant' }],
-    maintenance: [],
+    maintenance: [Maintenance],
     owner: { type: {}, required: true },
+    closed: { type: Boolean, required: true, default: false },
     creatorId: { type: String, required: true },
-    maintenanceId: { type: ObjectId, ref: 'Maintenance', required: true },
     ownerId: { type: ObjectId, ref: 'Owner', required: true }
   },
   { timestamps: true, toJSON: { virtuals: true } }
@@ -27,6 +28,13 @@ const Rental = new Schema(
 Rental.virtual('creator', {
   localField: 'creatorId',
   ref: 'Account',
+  foreignField: '_id',
+  justOne: true
+})
+
+Rental.virtual('maintenance', {
+  localField: 'maintenance',
+  ref: 'Maintenance',
   foreignField: '_id',
   justOne: true
 })
--- proper.server/server/services/RentalsService.js
+++ proper.server/server/services/RentalsService.js
@@ -3,7 +3,7 @@ import { BadRequest } from '../utils/Errors'
 
 class RentalsService {
   async find(query = {}) {
-    const rentals = await dbContext.Rentals.find(query)
+    const rentals = await dbContext.Rentals.find(query).populate('maintenance', 'tasks')
     if (!rentals) {
       throw new BadRequest()
     }
@@ -11,18 +11,40 @@ class RentalsService {
   }
 
   async findById(id) {
-    const rental = await dbContext.Rentals.findById({ _id: id })
+    const rental = await dbContext.Rentals.findById({ _id: id }).populate('maintenance', 'tasks')
     if (!rental) {
       throw new BadRequest(`Sorry, but you have sent the wrong ID ${id}. Please re-check the info you are sending through.`)
     }
+    return rental
   }
 
   async create(rental) {
-    const newRental = await dbContext.Rentals.create(rental)
+    const newRental = await (await dbContext.Rentals.create(rental)).populate('maintenance', 'tasks')
     if (!newRental) {
-      throw new BadRequest('You may be missing on of the required')
+      throw new BadRequest(`You may be missing one of the required properties ${rental}`)
     }
     return newRental
+  }
+
+  async remove(id, body) {
+    // passing the closed property through...even though they should not be able to send a body for the delete I am going to protect the data
+    delete body.closed
+    const rental = await dbContext.Rentals.findOneAndUpdate({ _id: id }, { closed: true }, { new: true })
+    if (!rental) {
+      throw new BadRequest('You can not actually delete this. Please send the ID and we will softly delete this data')
+    }
+    return rental
+  }
+
+  async update(id, body, userInfo) {
+    delete body.closed
+    const rental = await dbContext.Rentals.find({ _id: id }).populate('maintenance', 'tasks')
+    if (rental.creatorId !== userInfo.id) {
+      throw new BadRequest('You are not the creator of this rental and you do not have permissions to update this data.')
+    } if (rental && rental.closed) {
+      throw new BadRequest('This rental may already be closed, or you are trying to close a rental through the edit. If you are trying to close the rental please use the delete process.')
+    }
+    return await dbContext.Rentals.findOneAndUpdate({ _id: id }, body, { new: true })
   }
 }
 
