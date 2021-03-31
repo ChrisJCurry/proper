0f78fd535d0bd8ee262be0c9ccddf9dff563669d
Kurtis Eberhardt <77414699+KurtisEberhardt@users.noreply.github.com>
2021-03-31 16:06:23 -0600

    DesktopPage created, it's empty :)

---
+++ proper.client/src/pages/DesktopPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="desktop-page">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DesktopPage',
+  setup() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
