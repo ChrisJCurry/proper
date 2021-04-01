9b84956af8d28fe8b9358ce2dd0b4026337a9a0a
Kurtis Eberhardt <77414699+KurtisEberhardt@users.noreply.github.com>
2021-04-01 11:51:35 -0600

    desktop mode components no logic

---
+++ proper.client/src/components/DesktopRentalTableData.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="desktop-rental-table-data">
+    <tr>
+      <th scope="row">
+        Apartment Number Here
+      </th>
+      <td>
+        {{ tenant.address }}
+      </td>
+      <!--TODO NULL CHECK WITH V-IF-->
+      <td>
+        {{ tenant.name }}
+      </td>
+      <td>
+        {{ tenant.primarycontact }}
+      </td>
+      <td>
+        Avaliable? <!-- bool-->
+      </td>
+      <td>
+        <button class="btn btn-success">
+          E-Mail Tenant
+        </button>
+      </td>
+    </tr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DesktopRentalTableData',
+  props: {
+    tenant: { type: Object, required: true }
+  },
+  setup() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
