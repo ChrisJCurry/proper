443322c2b4f187e3c7d1559b361d77fb59e11d88
Chris J Curry <cjcurrybusiness@gmail.com>
2021-04-01 19:09:01 -0600

    Added sorts for tasks page.

--- proper.client/src/pages/TasksPage.vue
+++ proper.client/src/pages/TasksPage.vue
@@ -11,19 +11,42 @@
         </h4>
       </div>
     </div>
-
+    <div class="row">
+      <div class="col-2 offset-1 text-center border border-dark">
+        <button type="button" class="btn btn-dark" @click="sortByComplete">
+          Completed
+        </button>
+      </div>
+      <div class="col-2 text-center border border-dark">
+        <button type="button" class="btn btn-dark" @click="sortByDueDate">
+          Due Date
+        </button>
+      </div>
+      <div class="col-3 text-center border border-dark">
+        <button type="button" class="btn btn-dark" @click="sortByCreationDate">
+          Created Date
+        </button>
+      </div>
+    </div>
     <div class="row" v-if="state.tasks">
       <div class="col-12 mt-2" v-for="task in state.tasks" :key="task.id">
         <div v-if="task.closed">
           <div class="card overlay">
             <div class="card-body shadow">
               <div class="row">
+                <div class="col-1">
+                  <button class="btn btn-dark" @click="toggleTask(task)">
+                    <h6 class="m-0">
+                      reactivate
+                    </h6>
+                  </button>
+                </div>
                 <div class="col-3">
                   <div class="card-text text-right">
                     <h6>{{ task.title }}: </h6>
                   </div>
                 </div>
-                <div class="col-8">
+                <div class="col-7">
                   <div class="card-text">
                     <h6>{{ task.description }}</h6>
                   </div>
@@ -43,13 +66,6 @@
               </div>
               <div class="row text-center">
                 <div class="col-6">
-                  <button class="btn btn-dark" @click="toggleTask(task)">
-                    <h6 class="m-0">
-                      reactivate
-                    </h6>
-                  </button>
-                </div>
-                <div class="col-6">
                   <small>{{ task.dueDate }}</small>
                 </div>
               </div>
@@ -60,12 +76,19 @@
           <div class="card">
             <div class="card-body shadow">
               <div class="row">
+                <div class="col-1">
+                  <button class="btn btn-success" @click="toggleTask(task)">
+                    <h6 class="m-0">
+                      complete
+                    </h6>
+                  </button>
+                </div>
                 <div class="col-3">
                   <div class="card-text text-right">
                     <h6>{{ task.title }}: </h6>
                   </div>
                 </div>
-                <div class="col-9">
+                <div class="col-8">
                   <div class="card-text">
                     <h6>{{ task.description }}</h6>
                   </div>
@@ -79,13 +102,6 @@
                 </div>
               </div>
               <div class="row text-center">
-                <div class="col-6">
-                  <button class="btn btn-success" @click="toggleTask(task)">
-                    <h6 class="m-0">
-                      complete
-                    </h6>
-                  </button>
-                </div>
                 <div class="col-6">
                   <small>{{ task.dueDate }}</small>
                 </div>
@@ -106,7 +122,9 @@ export default {
   setup() {
     const state = reactive({
       // NOTE use this to get all tasks for your rentals
-      tasks: computed(() => AppState.tasks)
+      tasks: computed(() => AppState.tasks),
+      dueDateToggle: false,
+      creationDateToggle: false
     })
     onMounted(async() => {
       await tasksService.getAll()
@@ -120,6 +138,31 @@ export default {
       async removeTask(task) {
         await tasksService.delete(task)
         await tasksService.getAll()
+      },
+      async sortByComplete() {
+        state.tasks.sort((a, b) => a.closed === b.closed ? 0 : a ? -1 : 1)
+      },
+      async sortByDueDate() {
+        if (state.dueDateToggle) {
+          state.tasks.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate))
+          state.dueDateToggle = false
+          // eslint-disable-next-line no-useless-return
+          return
+        } else {
+          state.tasks.sort((a, b) => new Date(b.dueDate) - new Date(a.dueDate))
+          state.dueDateToggle = true
+        }
+      },
+      async sortByCreationDate() {
+        if (state.creationDateToggle) {
+          state.tasks.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
+          state.creationDateToggle = false
+          // eslint-disable-next-line no-useless-return
+          return
+        } else {
+          state.tasks.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+          state.creationDateToggle = true
+        }
       }
     }
   },
@@ -132,5 +175,8 @@ export default {
   height: 100%;
   background-color: gray;
   opacity: .8;
+}
+button {
+  user-select: none;
 }
 </style>
