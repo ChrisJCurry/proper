3971ae5ef97658b8c576ef4c53176e09a95c8df4
George Condit <georgecondit@mac.com>
2021-04-02 12:18:52 -0600

    Task Modal works on desk and mobile

--- proper.client/src/components/CreateTaskModal.vue
+++ proper.client/src/components/CreateTaskModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="create-task-modal">
+  <div class="create-task-modal" v-if="state.viewportWidth <= 700">
     <div class="modal fade"
          id="create-task"
          tabindex="-1"
@@ -46,10 +46,60 @@
       </div>
     </div>
   </div>
+  <div class="create-task-modal" v-else>
+    <div class="modal fade"
+         id="create-task"
+         tabindex="-1"
+         role="dialog"
+         aria-labelledby="modelTitleId"
+         aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header bg-primary border-bottom">
+            <h4 class="modal-title">
+              New Task
+            </h4>
+            <span type="button" class="close py-3" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </span>
+          </div>
+
+          <div class="modal-body">
+            <form class="form-inline" @submit.prevent="createTask">
+              <div>
+                <input class="mb-3" type="text" placeholder="Task Title" v-model="state.task.title">
+              </div>
+              <div>
+                <textarea name="task-text"
+                          id="task-description"
+                          cols="30"
+                          rows="5"
+                          placeholder="Add a Task"
+                          aria-describedby="helpId"
+                          v-model="state.task.description"
+                          required
+                >
+                  </textarea>
+                <div class="form-group">
+                  <small>Complete by: <Datepicker v-model="dueDate" /></small>
+                </div>
+                <div>
+                  <button class="btn btn-success mt-3" type="submit">
+                    Create
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { reactive, computed, ref } from 'vue'
+import { reactive, computed, onMounted, ref } from 'vue'
 import { logger } from '../utils/Logger'
 import $ from 'jquery'
 import { AppState } from '../AppState'
@@ -61,8 +111,12 @@ export default ({
     const state = reactive({
       task: {},
       tasks: computed(() => AppState.tasks),
-      rental: computed(() => AppState.rental)
+      rental: computed(() => AppState.rental),
+      viewportWidth: window.innerWidth
+
     })
+    onMounted(() => { window.addEventListener('resize', () => { state.viewportWidth = window.innerWidth }) })
+
     return {
       state,
       dueDate,
